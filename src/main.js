61f3de59b840b25862dcbd091b83da26fd345571
Alejandro Martinez <alejandro.martinez02@globant.com>
2025-10-01 16:09:36 +0200

    añadiendo la parte 2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
@@ -12,7 +11,38 @@ boton.addEventListener("click",function(){
   var valorNombre = inputTarea.value;
   var valorDescripcion = inputDescripcion.value;
   var li = document.createElement("li")
+  var selecion = document.createElement("select")
+  var opcionCompletado = document.createElement("option");
+  opcionCompletado.value="completado"
+  opcionCompletado.textContent="completado";
+  var opcionPendiente = document.createElement("option");
+  opcionPendiente.value="pendiente"
+  opcionPendiente.textContent="pendiente"
+  //añadir las opciones del select
+  selecion.appendChild(opcionCompletado)
+  selecion.appendChild(opcionPendiente)
   li.textContent = valorNombre+"-"+valorDescripcion;
+  li.appendChild(selecion)
   lista.appendChild(li)
-  
+  guardarTareas();
 })
+
+//funcion guardar tareas para que simpre que añadimos algo se guarde
+function guardarTareas(){
+  var tareas = [];
+  //sacar todos los li de la lista
+  var items = lista.querySelectorAll("li"); 
+
+  //para cada li vamos a hacer
+  items.forEach(li => {
+    //separamos el texto en nombre y descripcion usando el guion
+    let [nombre, descripcion] = li.firstChild.textContent.split("-");
+    //sacamos el valor actual del select
+    let estado = li.querySelector("select").value;
+    //guardamos el objeto en el array
+    tareas.push({ nombre, descripcion, estado });
+  });
+
+  //guardamos el array de tareas como texto en localStorage
+  localStorage.setItem("listaDeTareas", JSON.stringify(tareas));
+}
